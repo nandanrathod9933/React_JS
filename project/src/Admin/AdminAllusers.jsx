2caa8d5690de3bea7b058442f94e56295b45603e
rathodnandan9933 <rathodnandan9933@gmail.com>
2023-06-01 23:20:15 +0530

    admin dashboard

--- project/src/Admin/AdminAllusers.jsx
+++ project/src/Admin/AdminAllusers.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
 
 const AdminAllusers = () => {
+
+
+
+
     return (
-        <div>
-            AdminAllusers
-        </div>
+        <>
+            <section>
+                <h2 className='text-center text-capitalize fs-1'>user data</h2>
+
+                <table className='table mt-5' >
+                    <thead className='bg-dark text-light'>
+                        <tr>
+                            <th>No.</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>id</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>test</td>
+                            <td>test@test.com</td>
+                            <td>1</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </section>
+        </>
     );
 };
 
--- project/src/Admin/AdminMenu.jsx
+++ project/src/Admin/AdminMenu.jsx
@@ -12,18 +12,42 @@ const AdminMenu = () => {
     return (
         <>
             <aside id="sidebar" className={aside ? "sidebaractive" : ""}>
-                <div className="sidemenu" >
-                    <ul>
-                        <li><Link to='adminallusers'>adminallusers</Link></li>
-                        <li><Link to='admindashboard'>admindashboard</Link></li>
+                <h1 className='border-bottom'>Admin</h1>
+                <div className="sidemenu mt-3" >
+                    <ul className='ps-0'>
+                        <li><Link to='admindashboard'> dashboard</Link></li>
+                        <li><Link to='adminallusers'>All Users</Link></li>
                     </ul>
 
                 </div>
             </aside >
 
             <main id="main" className={aside ? "mainactive" : ""}>
-                <h2 id="btnclick" onClick={btnclick}><i className={aside ? "fa-solid fa-times" : "fa-solid fa-bars"}></i></h2>
-                <Outlet></Outlet>
+                <header className='hedaer'>
+                    <div className="flex align-items-center">
+                        <div className="col">
+                            <h2 id="btnclick" onClick={btnclick} className=' p-2 mb-0'><i className={aside ? "fa-solid fa-times" : "fa-solid fa-bars"}></i></h2>
+                        </div>
+                        <div className="col">
+                            <div className="flex align-items-center justify-content-between">
+                                <div className="input_box">
+                                    <input type="text" className='dash_input' />
+                                    <i class="fa-solid fa-search"></i>
+                                </div>
+                                <div className="list me-5">
+                                    <ul className='mb-0'>
+                                        <li><i class="fa-solid fa-user"></i></li>
+                                        <li><i class="fa-solid fa-house"></i></li>
+                                        <li><i class="fa-solid  fa-bell"></i></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </header>
+                <section className='px-2 pt-3'>
+                    <Outlet></Outlet>
+                </section>
             </main>
 
 
